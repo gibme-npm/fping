ebdd68061d2df681939bde8688f39b1755ef8da3
Brandon Lehmann <brandonlehmann@gmail.com>
2025-05-10 19:54:38 -0400

    - updated dependencies - merged types into namespace

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, Brandon Lehmann <brandonlehmann@gmail.com>
+// Copyright (c) 2024-2025, Brandon Lehmann <brandonlehmann@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,9 +21,6 @@
 import { exec } from 'child_process';
 import { Address4, Address6 } from 'ip-address';
 import which from 'which';
-import { Options, Result, ResultSet } from './types';
-
-export { Options, Result, ResultSet };
 
 /** @ignore */
 const toFixed = (value: number, digits = 3): number =>
@@ -50,8 +47,8 @@ const checkIp = (value: string): string => {
  */
 const fping = async (
     targets: string[],
-    options: Partial<Options> = {}
-): Promise<ResultSet> => {
+    options: Partial<fping.Options> = {}
+): Promise<Record<string, fping.Result | undefined>> => {
     options.bytes ??= 56;
     options.backoff ??= 1.5;
     options.count ??= 1;
@@ -123,7 +120,7 @@ const fping = async (
                     .filter(line => line.length !== 0)
                     .map(line => line.split(':').map(col => col.trim()));
 
-                const result: ResultSet = {};
+                const result: Record<string, fping.Result | undefined> = {};
 
                 for (const [host, response] of lines) {
                     const times = response.split(' ')
@@ -150,5 +147,82 @@ const fping = async (
                 return resolve(result);
             }));
 };
+
+export namespace fping {
+    export type Options = {
+        /**
+         * Amount of ping data to send, in bytes
+         *
+         * @default 56
+         */
+        bytes: number;
+        /**
+         * The exponential backoff factor
+         *
+         * @default 1.5
+         */
+        backoff: number;
+        /**
+         * Send this many pings to each target
+         *
+         * @default 1
+         */
+        count: number;
+        /**
+         * Interval between sending ping packets (in milliseconds)
+         *
+         * @default 10
+         */
+        interval: number;
+        /**
+         * Interval between ping packets to one target (in milliseconds)
+         *
+         * @default 1000
+         */
+        period: number;
+        /**
+         * Number of retries
+         *
+         * @default 3
+         */
+        retry: number;
+        /**
+         * Random packet data (to foil link data compression)
+         *
+         * @default true
+         */
+        random: boolean;
+        /**
+         * Individual target initial timeout (in milliseconds)
+         *
+         * @default 500
+         */
+        timeout: number;
+        /**
+         * Set the digit count of latency times
+         *
+         * @default 3
+         */
+        digits: number;
+        /**
+         * Set the digit count of loss percentage
+         *
+         * @default 4
+         */
+        lossDigits: number;
+    }
+
+    export type Result = {
+        target: string;
+        sent: number;
+        received: number;
+        loss: number;
+        avg: number;
+        min: number;
+        max: number;
+        stddev: number;
+        times: number[];
+    }
+}
 
 export default fping;
